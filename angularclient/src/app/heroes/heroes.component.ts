0118b608f49b4ada8ac38e2254d2f14ec11c0e73
StealingDaPenta <tomdescheemaeker@hotmail.com>
2024-10-21 19:07:01 +0200

    Updated interactive heroes page with freefilled list

--- angularclient/src/app/heroes/heroes.component.ts
+++ angularclient/src/app/heroes/heroes.component.ts
@@ -1,23 +1,27 @@
 import {Component} from '@angular/core';
-import {Hero} from '../interface/hero';
-import {UpperCasePipe} from '@angular/common';
+import {NgForOf, UpperCasePipe} from '@angular/common';
 import {FormsModule} from '@angular/forms';
+import {HEROES} from '../const/mock-heroes';
+import {Hero} from '../interface/hero';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
   imports: [
     UpperCasePipe,
-    FormsModule
+    FormsModule,
+    NgForOf
   ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
+  public heroes: Hero[] = HEROES;
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
 }
